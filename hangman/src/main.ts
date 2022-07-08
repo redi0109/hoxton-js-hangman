822cb13c8f9a853a802d627d036a6aa49c0a9763
Demir Boraj <demirboraj97@gmail.com>
2022-07-08 18:40:43 +0200

    no repeated characters

--- hangman/src/main.ts
+++ hangman/src/main.ts
@@ -6,24 +6,70 @@ type Words = string[]
 type State = {
   word: string,
   characters: string[],
-  mistakeCount: number
+  maxMistakes: number,
+  // guessed: boolean
 }
 
 let words: Words = ['apple', 'banana', 'orange', 'pineapple', 'strawberry', 'watermelon'] 
 
 let state: State = {
-  word: '',
+  word: 'apple',
   characters: [],
-  mistakeCount: 0
+  maxMistakes: 6,
+  // guessed: false
+}
+
+function getMistakes(){
+  let mistakes = state.characters.filter(char => !state.word.includes(char)) 
+  return mistakes
 }
 
 function getMistakeCount(){
-  let mistakeCount = state.characteres.filter(char => !state.word.includes(char)) //includes metode per te gjetur nese nje karakter i perket fjales
-  return mistakeCount
+  let mistakes = getMistakes()
+  return mistakes.length
 }
+
+function ifUserWon(){
+  if (getMistakeCount() === 0) {
+    alert('You won!')
+  }
+}
+
+function ifUserLost(){
+  if (getMistakeCount() === state.maxMistakes) {
+    alert('You lost!')
+  }
+}   
 
 
 
 function render(){
+let appEl = document.querySelector('#app')
+if (appEl === null) return
 
+appEl.textContent = ''
+
+let mistakesSpan = document.createElement('span')
+mistakesSpan.textContent = `Mistakes: ${getMistakes()} ${getMistakeCount()}`
+appEl.append(mistakesSpan)
 }
+
+function getKeyPressesFromUser (){
+  document.addEventListener('keyup', function(event) {
+
+    let letters = 'abcdefghijklmnopqrstuvwxyz'
+    if (!letters.includes(event.key)) return
+
+    //no repeated charcters
+    if (state.characters.includes(event.key)) return
+    state.characters.push(event.key)
+       render()
+  })
+  }
+
+getKeyPressesFromUser ()
+render()
+
+window.state = state
+window.getMistakes = getMistakes
+window.getMistakeCount = getMistakeCount
